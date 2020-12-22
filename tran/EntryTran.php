e465172f340155c02c8bb668f377f083c24cb0b1
Sabik safkat (windows) <safkatsabik@gmail.com>
2020-12-22 19:35:51 +0600

    sync

--- tran/EntryTran.php
+++ tran/EntryTran.php
@@ -64,13 +64,14 @@ class EntryTran extends BaseTran {
 	            ("3Dセキュア店舗名のBASE64暗号化後の長さが25Byteを超えています。", $this->exception);
 	        return null;
 	    }
-        echo '<pre>';
-        print_r($encodeTdTenantName);
-        exit;
+
 	    // 暗号化文字列を再セット
 		$input->setTdTenantName($encodeTdTenantName);
         // 接続しプロトコル呼び出し・結果取得
         $resultMap = $this->callProtocol($input->toString());
+        echo '<pre>';
+        print_r($resultMap);
+        exit;
         // 店舗名を退避しておいた元の文字列に戻す
         $input->setTdTenantName($tdTenantName);
 	    // 戻り値がnullの場合、nullを戻す
